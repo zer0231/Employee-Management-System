51759e023c5b99c08254afacbe15a2fa7f530721
zer0231 <pranay_zer0@protonmail.com>
2023-01-18 09:34:23 +0545

    Added model

--- EMS/EMS_Site/models.py
+++ EMS/EMS_Site/models.py
@@ -1,3 +1,21 @@
+from statistics import mode
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+
+class employee_detail(models.Model):
+    GENDER = [("M","Male"),("F","Female"),("O","others")]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=13,blank=True)
+    email = models.EmailField(max_length=255)
+    address = models.TextField()
+    status= models.BooleanField()
+    gender = models.CharField(choices=GENDER,max_length=1)
+    dob = models.DateField()
+    joining_date = models.DateField()
+    jobs = models.ManyToManyField("job",blank=True)
+
+class job(models.Model):
+    name = models.CharField(max_length=100)
